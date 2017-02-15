23b192c59e7c7577ed0dfd3f8c735d1d0ff61286
Sachin Lohith <sachinlohith93@gmail.com>
2017-02-14 17:29:08 -0800

    Markers for all locations

---
+++ geocoding.py
@@ -0,0 +1,28 @@
+# A sample Python module to geocode the locations mentioned in nodes.txt
+
+import geocoder
+import json
+
+fileName = "Intertubes-Dataset/nodes.txt"
+output = "geocoded.json"
+
+data = []
+with open(fileName) as fp:
+    data = fp.read().split("\n")
+
+data = data[:-1]
+nodes = {}
+index = 1
+for line in data:
+    print index
+    node, location = line.replace(';', ", ").split('\t')
+    geocode = geocoder.google(location)
+    nodes[int(node)] = {
+        'location': location,
+        'geocode': geocode.latlng
+    }
+    index += 1
+
+with open(output, "w") as fp:
+    json.dump(nodes, fp) 
+    json.dump("\n", fp)
